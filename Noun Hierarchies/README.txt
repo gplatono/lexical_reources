a3c7a3ae8294d61c53c73a54af0624701c22e2d9
agindi <aarongindi@gmail.com>
2019-07-26 16:21:41 -0400

    Updates

--- Noun Hierarchies/README.txt
+++ Noun Hierarchies/README.txt
@@ -1,13 +1,36 @@
 This folder contains resources for exploring and building noun hierarchies:
 Sources:
   contains noun lists for generating hierarchies or more selective lists
+
 Hierarchies:
   contains the hierarchies generated from the noun lists
+
 generate_hiers.py:
-  a script which allows for hierarchy generation from the list of source nouns.  Use the configuration variables at the beginning of the file.
+  a script which allows for hierarchy generation from the list of source nouns.
+  usage: To set any of the following flags, put "-flag true" or "-flag false", and for thresholds just put a number
+-f: filter, i.e. should any nodes be removed to condense the tree
+-ftt: filter trimming threshold, 0 < x < 100, if f is enabled it will remove words that occur less than x times in the Brown corpus.  It depends, but generally 0 is 0%, 1 is ~20%, 15 is ~35%, etc.
+
+-fim: filter inexact matches, i.e. ignore the words which aren't the name of the wordnet synset
+
+-fis: filter infrequent senses, i.e. remove the senses that aren't of at least a certain frequency of total uses in the Semcor corpus
+-stt: sense trimming threshold, 0 < x < 1, if fis is enabled then senses that occur less than (x*100)% of the time are removed
+
+-fne: filter named entities, i.e. remove the synsets which correspond to named entities
+
+-fsp: filter single parents, i.e. remove the synsets which have exactly one child in the hierarchy
+
+-i: use this to set the input file
+
+-o: use this to set the output file
+-epi: use epilog hierarchy output
+
+-d: enables debugging mode, it prints a lot of things in the console
+
+example: py generate_hiers.py -i ogden_nouns.txt -o on_hierarchy.txt -f true -ftt 10 -fis false -fim true -epi true
+
 selecting_synsets_script.py:
   a script for manual filtering or selection of senses of the words of a particular list, allows for much more selective hierarchy generation at the cost of being quite labor intensive, though the script tries to make is easier
+
 toy_script.py:
   this was for some initial exploration, still some useful bits but generally avoid using
-
-The generated hierarchies, when using default output file name None, are outputted to the file "[file_in]_hierarchy_x.txt", where x is the least integer for which the file doesn't already exist.
--- Noun Hierarchies/generate_hiers.py
+++ Noun Hierarchies/generate_hiers.py
@@ -1,10 +1,12 @@
 import io
 import os
+import sys
 from semcor_sense_frequency import SemcorSenseFrequency
 from brown_frequency import BrownFrequency
 from nltk.corpus import wordnet as wn
 from nltk.corpus import brown
 from nltk import FreqDist as FD
+import ast
 from anytree import Node, RenderTree, AsciiStyle, PreOrderIter
 '''
 This script is for creating a heirarcy from any list of nouns.
@@ -36,11 +38,12 @@ selection of synsets but dramatically decreases the complexity of the tree
 # CONFIG ------------------------------------------------------------------------------------------
 # I/O -------------------------------------------
 # the input file
-file_in = "gathered_nouns.txt"
+file_in = "ogden_nouns_manually_selected.txt"
 
 # the output file
 file_out = None
 use_output = True
+epilog_output = False
 
 # Filtering Flags -------------------------------
 # should we use only exact matches?
@@ -59,16 +62,20 @@ filter_named_entities = True
 filter_single_parents = True
 
 # the frequency cutoff to filter the inner nodes by
-trimming_threashold = 15
+trimming_threashold = 5
 # 0 is 0%, 1 is ~20%, 15 is ~35%, etc.
 
 # the frequency cutoff to filter the infrequent senses by
 sense_trimming_threashold = 0.2
 
+# how do we want to handle the multiple hypernyms cases?
+use_more_frequent_parent = True
+use_arbitrary_parent = False
+
 # Printing / Debug Flags ------------------------
 print_synsets = False   # determines if the synsets & definitions are printed
 print_paths = False      # determines if the path for each word is printed
-print_numbers = True    # determines if various numbers are printed
+print_numbers = False    # determines if various numbers are printed
 print_tree_one = False    # determines if we print out the tree
 print_tree_two = False    # determines if we print out the tree after trimming
 
@@ -100,6 +107,62 @@ def get_flags():
         out += "f"
     out += str(trimming_threashold)
     return out
+
+# Check for args ----------------------------------------------------------------------------------
+if len(sys.argv) > 1:
+    if "-fim" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-fim"))].lower()[0] == "t":
+            filter_inexact_matches = True
+        else:
+            filter_inexact_matches = False
+    if "-fis" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-fis"))].lower()[0] == "t":
+            filter_infrequent_senses = True
+        else:
+            filter_infrequent_senses = False
+    if "-fne" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-fne"))].lower()[0] == "t":
+            filter_named_entities = True
+        else:
+            filter_named_entities = False
+    if "-fsp" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-fsp"))].lower()[0] == "t":
+            filter_single_parents = True
+        else:
+            filter_single_parents = False
+    if "-f" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-f"))].lower()[0] == "t":
+            filter_inner_nodes = True
+        else:
+            filter_inner_nodes = False
+
+    if "-ftt" in sys.argv:
+        trimming_threashold = int(sys.argv[(1+sys.argv.index("-ftt"))].lower())
+
+    if "-stt" in sys.argv:
+        sense_trimming_threashold = float(sys.argv[(1+sys.argv.index("-stt"))].lower())
+
+    if "-epi" in sys.argv:
+        if sys.argv[(1+sys.argv.index("-epi"))].lower()[0] == "t":
+            epilog_output = True
+        else:
+            epilog_output = False
+
+    if "-i" in sys.argv:
+        file_in = sys.argv[(1+sys.argv.index("-i"))].lower()
+
+    if "-o" in sys.argv:
+        file_out = sys.argv[(1+sys.argv.index("-o"))].lower()
+    else:
+        file_out = None
+
+    if "-d" in sys.argv:
+        print_synsets = True
+        print_paths = True
+        print_numbers = True
+        print_tree_one = True
+        print_tree_two = True
+
 
 # Reading -----------------------------------------------------------------------------------------
 # storing the words
@@ -145,6 +208,9 @@ if print_numbers:
 # reading complete --------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
 
+# important long-time things
+brFreq = BrownFrequency()
+
 # Get Synsets -------------------------------------------------------------------------------------
 # a place to store them
 synsets = []
@@ -154,10 +220,6 @@ if clarify_synsets: # type 2
     for word in words:
         all_syns = wn.synsets(word, pos=wn.NOUN)
         for i in clarified_synsets[word]:
-
-            # if we're removing inexact matches, check if it maches and remove if not
-            if filter_inexact_matches and not synstr(all_syns[i-1]).split(".")[0] == word:
-                continue
 
             # otherwise append
             synsets.append(all_syns[i-1])
@@ -211,29 +273,71 @@ paths = []
 
 # for each synset make a path
 for syn in synsets:
-    hypernyms = syn.hypernyms()
-    synstring = synstr(syn)
-    path = [synstring]
+    if use_arbitrary_parent:
+        hypernyms = syn.hypernyms()
+        synstring = synstr(syn)
+        path = [synstring]
 
-    # climb up the tree
-    while not hypernyms == []:
-        next_syn = hypernyms[-1]
-        string_next = synstr(next_syn)
+        # climb up the tree
+        while not hypernyms == []:
+            next_syn = hypernyms[-1]
+            string_next = synstr(next_syn)
 
-        # ensures there's no cycles formed
-        if string_next in path:
-            break
+            # ensures there's no cycles formed
+            if string_next in path:
+                break
 
-        path.append(string_next)
+            path.append(string_next)
 
-        hypernyms = next_syn.hypernyms()
+            hypernyms = next_syn.hypernyms()
 
-    if print_paths:
-        print(synstring)
-        print("\t" + str(path))
+        if print_paths:
+            print(synstring)
+            print("\t" + str(path))
 
-    # add the valid path to the paths
-    paths.append(path)
+        # add the valid path to the paths
+        paths.append(path)
+    elif use_more_frequent_parent:
+
+        hypernyms = syn.hypernyms()
+        synstring = synstr(syn)
+        path = [synstring]
+
+        # climb up the tree
+        while not hypernyms == []:
+            if len(hypernyms) > 1:
+                dic = {word:brFreq.get_freq(synstr(word)) for word in hypernyms}
+                if all(x == 0 for x in dic.values()):
+                    mini = hypernyms[0]
+                    for i in range(1, len(hypernyms)):
+                        if len(synstr(hypernyms[i])) < len(synstr(mini)):
+                            mini = hypernyms[i]
+                    next_syn = mini
+                else:
+                    maxi = hypernyms[0]
+                    for i in range(1, len(hypernyms)):
+                        if dic.get(hypernyms[i]) > dic.get(maxi):
+                            maxi = hypernyms[i]
+                    next_syn = maxi
+            else:
+                next_syn = hypernyms[0]
+            string_next = synstr(next_syn)
+
+            # ensures there's no cycles formed
+            if string_next in path:
+                break
+
+            path.append(string_next)
+
+            hypernyms = next_syn.hypernyms()
+
+        if print_paths:
+            print(synstring)
+            print("\t" + str(path))
+
+        # add the valid path to the paths
+        paths.append(path)
+
 
 if print_numbers:
     print("PATHS: " + str(len(paths)))
@@ -304,8 +408,6 @@ if filter_named_entities:
 
 # do we want to filter the less frequent inner nodes?
 if filter_inner_nodes:
-    # get the brown frequency list
-    brFreq = BrownFrequency()
 
     # get the nodes using a preorder traversal
     preorder_tree_nodes = [node for node in PreOrderIter(root_node)]
@@ -315,7 +417,12 @@ if filter_inner_nodes:
 
     # now we need to eliminate the nodes that don't occur frequently enough
     for node in preorder_tree_nodes:
-        if brFreq.is_frequent(name(node), trimming_threashold):
+
+        # don't remove a leaf
+        if node.children == ():
+            continue
+
+        if not brFreq.is_frequent(name(node), trimming_threashold):
         # remove it from the tree
             parent = node.parent
 
@@ -323,9 +430,6 @@ if filter_inner_nodes:
             if parent == None:
                 continue
 
-            # don't remove a leaf
-            #if node.children == ():
-            #    continue
             # this is probably better
 
             inner_nodes_removed += 1
@@ -420,18 +524,40 @@ if os.path.exists(".\\Hierarchies\\" + file_out):
 		counter += 1
 	file_out = splits[0] + "_" + str(counter) + "." + splits[1]
 
-with open(".\\Hierarchies\\" + file_out, "w") as f:
-    f.write("Filter Inexact Synsets: " + str(filter_inexact_matches) + "\n")
-    f.write("Filter Infrequent Senses: " + str(filter_infrequent_senses) + "\n")
-    f.write("Filter Named Entities: " + str(filter_named_entities) + "\n")
-    f.write("Filter Inner Nodes: " + str(filter_inner_nodes) + "\n")
-    f.write("Filter Single Parents: " + str(filter_single_parents) + "\n")
-    f.write("WORDS:"+str(len(words))+"\n")
-    f.write("PATHS:"+str(len(paths))+"\n")
-    f.write("PRE-NODES:"+str(prenodes)+"\n")
-    f.write("POST-NODES:"+str(len(nodes))+"\n")
-    for pre, fill, node in RenderTree(root_node, style=AsciiStyle()):
-        f.write(("%s%s\n" % (pre, node.name)))
+if epilog_output:
+    def epilog_hier(node):
+        lis = [root_node, node]
+        for chil in node.children:
+            lis.append(chil)
+
+        out = "'" + lis[0].name + ((node.depth*4)*' ')
+        for i in range(1, len(lis)):
+            out += " '" + lis[i].name
+
+        return "(add-hier " + out + ")"
+
+    # for each node we want to output the node and the children
+    preorder_tree_nodes = [node for node in PreOrderIter(root_node)]
+
+    with open(".\\Hierarchies\\" + file_out, "w") as f:
+        for node in preorder_tree_nodes:
+            if node.children != ():
+                f.write(epilog_hier(node) + "\n")
+
+
+else:
+    with open(".\\Hierarchies\\" + file_out, "w") as f:
+        f.write("Filter Inexact Synsets: " + str(filter_inexact_matches) + "\n")
+        f.write("Filter Infrequent Senses: " + str(filter_infrequent_senses) + "\n")
+        f.write("Filter Named Entities: " + str(filter_named_entities) + "\n")
+        f.write("Filter Inner Nodes: " + str(filter_inner_nodes) + "\n")
+        f.write("Filter Single Parents: " + str(filter_single_parents) + "\n")
+        f.write("WORDS:"+str(len(words))+"\n")
+        f.write("PATHS:"+str(len(paths))+"\n")
+        f.write("PRE-NODES:"+str(prenodes)+"\n")
+        f.write("POST-NODES:"+str(len(nodes))+"\n")
+        for pre, fill, node in RenderTree(root_node, style=AsciiStyle()):
+            f.write(("%s%s\n" % (pre, node.name)))
 
 
 # done --------------------------------------------------------------------------------------------
